195c666ded635ed9fb5ff6c887476e12ae01e540
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2020-12-30 18:40:58 +0900

    ビューファイルの配置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.create(item_params)
+  end
+  
+  private item_params
+    params.require(:item).permit(:text,:price,:name,:category_id,:state_id,:delivery_fee_id,:prefecture_id,:delivery_by_days_id)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,6 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: "is invalid. Input Include both letters and numbers."
   validates :email, uniqueness: true
+
+  has_many :items
 end
