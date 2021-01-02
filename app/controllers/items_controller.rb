be4d240ec34649a01b558de13d7b8ac0fa01e85c
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-02 16:40:54 +0900

    未ログインユーザーのリダイレクト設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
-    @items = Item.all
   end
   def new
     @item = Item.new
   end
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
-  private item_params
-    params.require(:item).permit(:text,:price,:name,:category_id,:state_id,:delivery_fee_id,:prefecture_id,:delivery_by_days_id,:image)
+  private 
+  def item_params
+    params.require(:item).permit(:text,:price,:name,:category_id,:state_id,:delivery_fee_id,:prefecture_id,:delivery_by_days_id,:image).merge(user_id: current_user.id)
+  end
 end
