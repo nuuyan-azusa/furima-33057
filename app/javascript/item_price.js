93e937808b4c4a6ee45d9c57e875bfd61ac8ad44
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-04 11:11:06 +0900

    不必要な記述の削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -6,8 +6,6 @@ window.addEventListener('load', () => {
     const profitPrice = document.getElementById("profit");
     const value_result = inputValue * 0.1
     addTaxPrice.innerHTML = (Math.floor(inputValue * 0.1 ));
-    console.log(addTaxPrice);
     profitPrice.innerHTML = (Math.floor(inputValue - value_result ));
-    console.log(profitPrice);
   })
 });
