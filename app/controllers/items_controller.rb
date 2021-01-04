219a0576f3ea6cabf5fd9905d6ac7f7abbeec8ad
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-04 18:44:42 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    end
     if @item.destroy
       redirect_to root_path
     end
