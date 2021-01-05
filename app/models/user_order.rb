012306e00ad0de742667b0a90986158377db6d2a
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-05 15:59:22 +0900

    バリデーション記述

---
+++ app/models/user_order.rb
@@ -0,0 +1,17 @@
+class UserOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\d{1,11}/, message: "Phone number Input only number"}
+  end
+  validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+
+  def save
+    @order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: @order.id)
+  end
+end
