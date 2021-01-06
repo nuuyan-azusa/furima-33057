492abfc98dc1713f2fbf336430721554d668ea96
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-06 10:42:46 +0900

    バリデーションの追記

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,8 +1,9 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
     validates :city
     validates :house_number
