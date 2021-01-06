1fc896bd270126a58e085c2af77b0b38b7fd9efd
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-06 10:40:02 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
 before_action :authenticate_user!, only: [:index,:create]
+before_action :set_item, only: [:index, :create]
   def index
     @user_order = UserOrder.new
-    @item = Item.find(params[:item_id])
     if current_user == @item.user
       redirect_to root_path
     end
@@ -11,15 +11,29 @@ before_action :authenticate_user!, only: [:index,:create]
   def create
     @user_order = UserOrder.new(order_params)
      if @user_order.valid?
+      pay_item
       @user_order.save
-      redirect_to root_path
+      return redirect_to root_path
      else
-      render :index
+      render 'index'
      end
   end
 
   private
     def order_params
-      params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+      params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+    end
+
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
     end
 end
