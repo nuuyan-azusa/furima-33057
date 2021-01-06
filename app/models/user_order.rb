51d5789656803f92e709e528aeb1d9958584b91d
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-06 12:25:39 +0900

    テストコードの追記

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -7,7 +7,7 @@ class UserOrder
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /0[89]0\d{8}/, message: 'Phone number Input only number' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Phone number Input only number' }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
 
