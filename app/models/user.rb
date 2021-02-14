2d71d647fa4acae53edf274e284a3dfe2de9bbe9
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-02-14 16:18:03 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :first_name, format:         { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name,  format:         { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format:         { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+    validates :last_name,  format:         { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
     validates :first_name_reading,
-              format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+              format: { with: /\A[ァ-ヶー－]+\z/, message: 'を全角で入力してください' }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'を全角で入力してください' }
     validates :birthday
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Input Include both letters and numbers.'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'を英数混合で入力してください'
   validates :email, uniqueness: true
 
   has_many :items
